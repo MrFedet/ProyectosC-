6c330fe6a3a41cf30e42739c066843680b31ecc5
Federico Tahan <88520985+MrFedet@users.noreply.github.com>
2022-11-08 12:50:56 -0300

    Add files via upload

---
+++ Ejercitacion_tipo_parcial/Ejercitacion_tipo_parcial/1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Problema 2.8:
+Capturar los datos de 5 productos comprados en una tienda, incluyendo
+nombre, precio y cantidad en sus 3 listas respectivas, después calcular una cuarta
+lista con el gasto total por cada producto desplegarlo todo en un segundo panel e
+incluir también el gran total.*/
+
+namespace tarea
+{
+    public class producto
+    {
+        private string nombre;
+        private int cantidad;
+        private double precio;
+
+
+
+        public producto()
+        {
+            nombre = string.Empty;
+            cantidad = 0;
+            precio = 0;
+        }
+
+        public producto(string nombre, int cantidad, double precio)
+        {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+            this.precio = precio;
+        }
+
+        public string pNombre
+        {
+            set { nombre = value; }
+            get { return nombre; }
+        }
+
+        public double pPrecio
+        {
+            set { precio = value; }
+            get { return precio; }
+        }
+
+        public int pCantidad
+        {
+            set { cantidad = value; }
+            get { return cantidad; }
+        }
+    }
+}
---
+++ Ejercitacion_tipo_parcial/Ejercitacion_tipo_parcial/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Problema 2.8:
+Capturar los datos de 5 productos comprados en una tienda, incluyendo
+nombre, precio y cantidad en sus 3 listas respectivas, después calcular una cuarta
+lista con el gasto total por cada producto desplegarlo todo en un segundo panel e
+incluir también el gran total.*/
+
+namespace tarea
+{
+    class Program {
+        static void Main(string[] args){
+
+            lista_productos list_prod = new lista_productos();
+
+            list_prod.Meter_datos();
+            list_prod.Calcular_precioTotal();
+            list_prod.Mostrar_Lista();
+
+            Console.ReadKey();
+
+        }
+    }
+}
